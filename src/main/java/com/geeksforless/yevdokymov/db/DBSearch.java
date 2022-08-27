e5502cd44d8be0a770a5fc4defa0b6e534ff39ec
fucusifei <spelpog@gmail.com>
2022-08-27 14:58:54 +0300

    add first version DBSearch.java class

---
+++ src/main/java/com/geeksforless/yevdokymov/db/DBSearch.java
@@ -0,0 +1,76 @@
+package com.geeksforless.yevdokymov.db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DBSearch {
+    public void search(Connection connection, String searchWord, double result) throws SQLException {
+        switch (searchWord) {
+            case ">" -> {
+                try(PreparedStatement statement = connection.prepareStatement("SELECT * FROM ArithmeticExpression WHERE arithmeticExpressionResult > (?)")) {
+                    statement.setDouble(1, result);
+                    ResultSet a = statement.executeQuery();
+                    while (a.next()) {
+                        System.out.println(a.getString(1));
+                    }
+                }
+                finally {
+                    connection.close();
+                }
+            }
+            case  "<"-> {
+                try(PreparedStatement statement = connection.prepareStatement("SELECT * FROM ArithmeticExpression WHERE arithmeticExpressionResult < (?)")) {
+                    statement.setDouble(1, result);
+                    ResultSet a = statement.executeQuery();
+                    while (a.next()) {
+                        System.out.println(a.getString(1));
+                    }
+                }
+                finally {
+                    connection.close();
+                }
+            }
+            case "=" -> {
+                try(PreparedStatement statement = connection.prepareStatement("SELECT * FROM ArithmeticExpression WHERE arithmeticExpressionResult = (?)")) {
+                    statement.setDouble(1, result);
+                    ResultSet a = statement.executeQuery();
+                    while (a.next()) {
+                        System.out.println(a.getString(1));
+                    }
+                }
+                finally {
+                    connection.close();
+                }
+            }
+            case "<=" -> {
+                try(PreparedStatement statement = connection.prepareStatement("SELECT * FROM ArithmeticExpression WHERE arithmeticExpressionResult <= (?)")) {
+                    statement.setDouble(1, result);
+                    ResultSet a = statement.executeQuery();
+                    while (a.next()) {
+                        System.out.println(a.getString(1));
+                    }
+                }
+                finally {
+                    connection.close();
+                }
+            }
+            case  ">="-> {
+                try(PreparedStatement statement = connection.prepareStatement("SELECT * FROM ArithmeticExpression WHERE arithmeticExpressionResult >= (?)")) {
+                    statement.setDouble(1, result);
+                    ResultSet a = statement.executeQuery();
+                    while (a.next()) {
+                        System.out.println(a.getString(1));
+                    }
+                }
+                finally {
+                    connection.close();
+                }
+            }
+            default -> {
+                System.out.println("wrong search word");
+            }
+        }
+    }
+}
